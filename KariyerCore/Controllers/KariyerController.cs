b60128f7658ce39d58360aeb12bfcf91accb5e35
Ogulcan aslan <ogulcan.aslan@hotmail.com>
2022-03-01 20:16:36 +0300

    Add project files.

---
+++ KariyerCore/Controllers/KariyerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KariyerCore.Models;
+using KariyerCore.Data;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace KariyerCore.Controllers
+{
+    public class KariyerController : Controller
+    {
+        private readonly KariyerDbContext _context;
+        private readonly IWebHostEnvironment _webHost;
+
+        public KariyerController(KariyerDbContext context, IWebHostEnvironment webHost)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<BasvuruSahibi> applicants;
+            applicants = _context.BasvuruSahibis.ToList();
+
+
+            return View(applicants);
+        }
+
+        public IActionResult Create()
+        {
+            BasvuruSahibi basvuruSahibi = new BasvuruSahibi();
+            return View(basvuruSahibi);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(BasvuruSahibi basvuruSahibi)
+        {
+            _context.Add(basvuruSahibi);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ KariyerCore/Models/BasvuruSahibi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KariyerCore.Models
+{
+    public class BasvuruSahibi
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required,StringLength(50)]
+        public string AdSoyad { get; set; }
+
+        [Required, StringLength(10)]
+        public string Cinsiyet { get; set; }
+
+        [Required]
+        [Range(25,55,ErrorMessage ="Bu Pozisyona Yaş Uygun Değildir.")]
+        [DisplayName("Yas")]
+        public int Yas { get; set; }
+
+        [Required]
+        [Range(1, 25, ErrorMessage = "Geçersiz")]
+        [DisplayName("Toplam Çalışma Yılı")]
+        public int ToplamTecrube { get; set; }
+
+
+    }
+}
